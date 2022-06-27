4314cfbac853cefb4c0b84c40691b2f14628db0f
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-27 20:51:57 +0900

    Button and images

--- UserInterface/UserInterface/ContentView.swift
+++ UserInterface/UserInterface/ContentView.swift
@@ -9,25 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .trailing, spacing: 200){
-            ZStack {
-                Color.blue
-                HStack {
-                Text("First")
-                Text("Second")
-                Text("Third")
+        HStack {
+            Image(systemName: "sun.max.fill")
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50, alignment: .center)
+                .padding()
+            Image("test")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 100, alignment: .center)
+                .padding()
+            Button {
+                print("Button Pressed")
+                didTapButton()
+            } label: {
+                ButtonContent(title: "Button Do Something")
             }
-            }
-            Spacer()
-            
-            ZStack {
-                Text("Second Vertical Stack")
-                
-            }
-            Spacer()
+
         }
     }
+    
+    
+    func didTapButton() {
+        
+    }
 }
+
+
+
+struct ButtonContent: View {
+    
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.system(size: 24))
+            .bold()
+            .frame(width: 220, height: 50, alignment: .center)
+            .background(Color.pink)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
